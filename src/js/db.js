99c10a8d7b7b86eca60fab9980ba2fa01a726572
Richard Cândido <rich_dacan@protonmail.com>
2022-07-03 18:37:57 -0300

    feat :sparkles: add link to repository

---
+++ src/js/db.js
@@ -0,0 +1,8 @@
+const cards = [
+  {
+    id: 1,
+    name: "Rick and Morty API",
+    description:
+      "Esse projeto foi estruturado visando fortalecer os conhecimentos básicos em React enquanto aprendia a utilizar uma ferramenta de requisições em apis chamada axios.",
+  },
+];
