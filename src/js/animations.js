ba1e853b5cf0b867a65de6d42e19c90360efefca
Richard Cândido <rich_dacan@protonmail.com>
2022-07-10 11:43:04 -0300

    style :gem: update animations

--- src/js/animations.js
+++ src/js/animations.js
@@ -1,17 +1,29 @@
-var slideLeft = {
-  distance: "150%",
-  origin: "left",
-  delay: 500,
-  opacity: null,
-  scale: 0.85,
-  easing: "steps(12)",
-};
-var slideRight = {
-  distance: "150%",
-  origin: "right",
-  delay: 500,
-  opacity: null,
-};
+// var slideLeft = {
+//   distance: "150%",
+//   origin: "left",
+//   delay: 500,
+//   opacity: null,
+//   scale: 0.85,
+//   easing: "steps(12)",
+// };
+// var slideRight = {
+//   distance: "150%",
+//   origin: "right",
+//   delay: 500,
+//   opacity: null,
+// };
 
-ScrollReveal().reveal(".column ", slideLeft);
+// ScrollReveal().reveal(".column ", slideLeft);
 // ScrollReveal().reveal(".slide__left", slideRight);
+window.sr = ScrollReveal({ reset: true });
+ScrollReveal({ distance: "15px" });
+sr.reveal(".home-content", { duration: 4000, origin: "left" });
+// sr.reveal(".logo", { duration: 3000, origin: "left" });
+// sr.reveal(".toggle", { duration: 3000, origin: "top" });
+// sr.reveal(".menu", { duration: 3000, origin: "right" });
+sr.reveal("#slide__right", { duration: 3000, origin: "right" });
+sr.reveal("#slide__left", { duration: 3000, origin: "left" });
+sr.reveal("#services", { duration: 3000, origin: "bottom" });
+sr.reveal(".contact-content", { duration: 3000, origin: "left" });
+// sr.reveal('.project', {duration : 3000, origin:'bottom'})
+// sr.reveal('.github-link', {duration : 3000, origin:'bottom'})
