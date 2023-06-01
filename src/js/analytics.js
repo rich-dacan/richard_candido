438ed4dd6604bb0f8f254467717aaefa0410d28d
Richard Cândido <rich_dacan@protonmail.com>
2023-06-01 15:11:09 -0300

    feat :sparkles: created more events

--- src/js/analytics.js
+++ src/js/analytics.js
@@ -1,7 +1,11 @@
 const github = document.getElementById("github");
+const linkedin = document.getElementById("linkedin");
+const cvDownload = document.getElementById("cv-download");
 
-const trackFunction = () => {
-  gtag("event", "github", { debug_mode: true });
+const trackFunction = eventName => {
+  gtag("event", eventName, { debug_mode: true });
 };
 
-github.addEventListener("click", trackFunction);
+github.addEventListener("click", trackFunction("github"));
+linkedin.addEventListener("click", trackFunction("linkedin"));
+cvDownload.addEventListener("click", trackFunction("cv-download"));
