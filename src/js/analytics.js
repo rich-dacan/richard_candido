4890087c4aa3e65bdab46f464e7eea12439aabb1
Richard Cândido <rich_dacan@protonmail.com>
2023-06-01 15:23:52 -0300

    fix :bug: fixed analytics file

--- src/js/analytics.js
+++ src/js/analytics.js
@@ -2,10 +2,24 @@ const github = document.getElementById("github");
 const linkedin = document.getElementById("linkedin");
 const cvDownload = document.getElementById("cv-download");
 
-const trackFunction = eventName => {
-  gtag("event", eventName, { debug_mode: true });
+const githubTrack = () => {
+  gtag("event", "github", { debug_mode: true });
+};
+const linkedinTrack = () => {
+  gtag("event", "linkedin", { debug_mode: true });
+};
+const cvTrack = () => {
+  gtag("event", "cv-download", { debug_mode: true });
 };
 
-github.addEventListener("click", trackFunction("github"));
-linkedin.addEventListener("click", trackFunction("linkedin"));
-cvDownload.addEventListener("click", trackFunction("cv-download"));
+github.addEventListener("click", githubTrack);
+linkedin.addEventListener("click", linkedinTrack);
+cvDownload.addEventListener("click", cvTrack);
+
+// const trackFunction = eventName => {
+//   gtag("event", eventName, { debug_mode: true });
+// };
+
+// github.addEventListener("click", trackFunction("github"));
+// linkedin.addEventListener("click", trackFunction("linkedin"));
+// cvDownload.addEventListener("click", trackFunction("cv-download"));
